9d9628b77e5f4c58681a03698dad70a0a5c35731
adolfogante <adolfogante@gmail.com>
2022-06-19 02:29:27 -0700

    comment description

--- Artiller.java
+++ Artiller.java
@@ -1,5 +1,19 @@
-package io.github.jiangdequan;
-
+/**
+ * Artiller.java: vidya
+ * 
+ * <p>
+ * <b>Discription</b>:<br /b>
+ * Artiller is a 2-D artillery game app where players shoot 
+ * down increasingly fast and numerous fleets of ships and planes 
+ * before they can reach the island’s shores. Between waves,
+ * the player can repair their base, increase its fortifications,
+ * buy missiles and order bombing runs, and attempt to win the game 
+ * by investing in a peace negotiation.
+ * </p>
+ * 
+ * @author Adolfo Gante
+ * @version 1.0
+ */
 public class Artiller {
     public static void  main (String[] args) {
 
